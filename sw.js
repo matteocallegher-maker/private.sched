03474a1d7158ab8fe758a72639f5c16ad7f92160
matteocallegher-maker <matteo.callegher@gmail.com>
2026-06-27 20:56:30 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+// Service worker minimo: serve solo a rendere l'app installabile
+// e a far funzionare la scheda anche offline dopo il primo caricamento.
+const CACHE_NAME = "callegher-v1";
+const FILES_TO_CACHE = ["./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
